92a45d5c8832014d19e63ba49b51deea785b442d
Norbert Melzer <timmelzer@gmail.com>
2026-03-04 07:55:32 +0100

    deal with macOS `hostname` particularities
    Change-Id: I3be9b393ac93b3966da1997dc4efff2a6a6a6964

--- src/system/mod.rs
+++ src/system/mod.rs
@@ -32,7 +32,12 @@ impl System {
     /// retrieved.
     #[instrument(skip(self))]
     pub async fn get_hostname(&self) -> Result<String, SystemError> {
-        self.get_command_out(&mut Command::new("hostname"))
+        #[cfg(target_os = "linux")]
+        let mut cmd = Command::new("hostname");
+        #[cfg(target_os = "macos")]
+        let mut cmd = Command::new("hostname").arg("-s");
+
+        self.get_command_out(&mut cmd)
             .await
             .map_err(|err| SystemError::HostnameError(Box::new(err)))
     }
