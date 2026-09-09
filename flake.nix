3e22db9afd72f5b08701710cefb3194071af02e7
Norbert Melzer <timmelzer@gmail.com>
2026-09-09 08:52:51 +0200

    update inputs
    Change-Id: I837a042df2b7940712a14224db875ce26a6a6964

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.zst";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
 
